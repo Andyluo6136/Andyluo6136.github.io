aec2962456562fbbf5ed9d1bbf36bd1beb955f45
Andy Luo <andyluo6136@gmail.com>
2026-08-15 17:43:35 -0400

    Update repo name to user pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/personal-website/',
+  base: '/',
   plugins: [react()],
   server: {
     open: true, // Automatically opens default browser on server start
